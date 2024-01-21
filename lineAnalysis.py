d2d4a0f1dc63674865812e31de548ed820763840
LordFurno <sirfurno@gmail.com>
2024-01-21 13:34:24 -0500

    main

---
+++ lineAnalysis.py
@@ -0,0 +1,102 @@
+import hapi
+import os
+import shutil
+import numpy as np
+import pandas as pd
+def convertCMtoUM(cm):#Convrets cm^-1 to microns
+   return 10000/cm 
+def getMoleculeData():
+    names={
+        1:"H2O",
+        2:"CO2",
+        3:"O3",
+        4:"N2O",
+        5:"CO",	
+        6:"CH4",
+        7:"O2",	
+        8:"NO",	
+        9:"SO2",	
+        10:	"NO2",	
+        11:	"NH3",	
+        12:	"HNO3",	
+        13:	"OH",
+        14:	"HF",
+        15:	"HCl",
+        16:	"HBr",
+        17:	"HI",
+        18:	"ClO",
+        19:	"OCS",
+        20:	"H2CO",
+        21:	"HOCl",
+        22:	"N2",
+        23:	"HCN",
+        24:	"CH3Cl",
+        25:	"H2O2",
+        26:	"C2H2",
+        27:	"C2H6",
+        28:	"PH3",
+        29:	"COF2",
+        30:	"SF6",
+        31:	"H2S",
+        32:	"HCOOH",
+        33:	"HO2",
+        34:	"O",
+        35:	"ClONO2",
+        36:	"NO+",
+        37:	"HOBr",
+        38:	"C2H4",
+        39:	"CH3OH",
+        40:	"CH3Br",
+        41:	"CH3CN",
+        42:	"CF4",
+        43:	"C4H3",
+        44:	"HC3N",
+        45:	"H2",
+        46:	"CS",
+        47:	"SO3",
+        48:	"C2N2",
+        49:	"COC12",
+        50:	"SO",
+        51:	"CH3F",
+        52:	"GeH4",
+        53:	"CS2",
+        54:	"CH3I",
+        55:	"NF3",
+    }  
+    for molecID in range(1,56):
+        try:#If wavelengths doesn't fit
+            hapi.fetch(names[molecID],molecID,1,1500,5000)#Just use the first isotopolouge, the most most abundant
+            curFilePath=r"C:\Users\Tristan\Downloads\ExoSeer" +f"\{names[molecID]}.data"
+            newFilePath=r"C:\Users\Tristan\Downloads\ExoSeer\Data\LineData"+f"\{names[molecID]}.data"
+
+            headerFilePath=r"C:\Users\Tristan\Downloads\ExoSeer" +f"\{names[molecID]}.header"
+            os.rename(curFilePath,newFilePath)
+            os.remove(headerFilePath)
+        except:
+            pass
+def dipFinder(filePath,molecule):#filePath is the exoplanet csv file. MOlecule is just the name of the molecule to find dips.
+    moleculeFilePath=r"C:\Users\Tristan\Downloads\ExoSeer\Data\LineData"+f"\{molecule}.data"
+
+
+    df=pd.read_csv(filePath)
+    wavelengths=df["CENTRALWAVELNG"]
+    transitDepths=df["PL_TRANDEP"]
+    derivative=np.gradient(transitDepths,wavelengths)
+    dipIndexes=np.where(derivative<0)[0]
+    dipLocation=[wavelengths[i] for i in dipIndexes]#Gets what wavelength they are at
+    dipValue=[transitDepths[i] for i in dipIndexes]
+
+    moleculeData=pd.read_csv(moleculeFilePath,sep="          ",header=None)#Sep is how the values are seperated in the data
+
+    molecWavelength=[]
+    molecIntensity=[]
+    for col in range(len(moleculeData)):
+        value=str(moleculeData.iloc[:,col][0])
+        value.split(' ')
+        molecWavelength.append(value[1])
+        molecIntensity.append(value[2])
+
+    
+    
+
+ 
