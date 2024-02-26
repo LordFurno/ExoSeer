de158aa3075e7d8c809e206ef5afa4b75a510cb9
LordFurno <sirfurno@gmail.com>
2024-02-25 21:14:20 -0500

    main

--- CNN.py
+++ CNN.py
@@ -27,105 +27,84 @@ def normalize(data,minVal,maxVal):
     scaledData=(data-data.min())/(data.max()-data.min())
     scaledData=scaledData*(maxVal-minVal)+minVal
     return scaledData
+def oneHotEncoding(label):
+    res=[0,0,0,0]
+    for value in label.split("-"):
+        if value=="N2":
+            res[0]=1
+        elif value=="O2":
+            res[1]=1
+        elif value=="CO2":
+            res[2]=1
+        elif value=="H2O":
+            res[3]=1
+    return res
 
-
-class moleculeDataset(Dataset):
-    def __init__(self,folderPath,molecule,transform=None):
-        self.folderPath=folderPath
-        self.molecule=molecule
-        self.csvFiles=[f for f in os.listdir(folderPath) if f.endswith('.csv')]
+class customDataset(Dataset):
+    def __init__(self,folderPaths,transform=None):
+        self.folderPaths=folderPaths
+        self.transform=transform
+        self.labels=[]
+        self.samples=[]
+        for label,directory in folderPaths:
+            files=os.listdir(directory)
+            for file in files:
+                filePath=os.path.join(directory,file)
+                self.samples.append(filePath)
+                self.labels.append(label)
         # print(len(self.csvFiles))
-        self.transform= transform
+        
     def __len__(self):
-        return len(self.csvFiles)
+        return len(self.samples)
     
     def __getitem__(self,index):
+        sample=self.samples[index]
+        label=self.labels[index]
+
         # print(index)
-        csvFile=self.csvFiles[index]
-        filePath=os.path.join(self.folderPath,csvFile)#Gets file path
-        data=loadCSV(filePath)
-        wavelength=data.iloc[:,1]#Wavelength column
-        molecData=data.iloc[:,3]#Transmittance column
+        data=loadCSV(sample)
 
-
+        wavelength=data.iloc[:,0]#Wavelength column
+        total=data.iloc[:,1]#Total transmittance column
 
         if self.transform:
-            wavelength,molecData=np.array(wavelength),np.array(molecData)
-            wavelength,molecData=wavelength.reshape(1,-1),molecData.reshape(1,-1)
+            wavelength,total=np.array(wavelength),np.array(total)
+            wavelength,totalData=wavelength.reshape(1,-1),total.reshape(1,-1)
             wavelength=self.transform(wavelength)
-            molecData=self.transform(molecData)
-            data=torch.stack([wavelength, molecData], dim=1)#Puts wavelength and molec data together in 1 tensor. 
-        return data,self.molecule#Wavelength, molecule,label
-class combineDataset(Dataset):
-    def __init__(self,dataset1,dataset2):
-        self.dataset1=dataset1
-        self.dataset2=dataset2
-        self.totaLength=len(dataset1)+len(dataset2)
+            totalData=self.transform(totalData)
+            data=torch.stack([wavelength, totalData], dim=1)#Puts wavelength and molec data together in 1 tensor. 
+        return data,label#Wavelength, label
 
-    def __len__(self):
-        return self.totaLength
-    def __getitem__(self,index):
-        if index<len(self.dataset1):
-            return self.dataset1.__getitem__(index)
-        else:
-            newIndex=index-len(self.dataset2)
-            return self.dataset2.__getitem__(newIndex)
+
 class Model(nn.Module):
-    def __init__(self):
+    def __init__(self):#Need to remake my entire model
         super().__init__()
-        self.conv1Molecule=nn.Conv2d(2,64,kernel_size=3,padding=1)
-        
-        self.pool=nn.MaxPool2d(kernel_size=(1,2),stride=(1,2))
-        
-        self.conv2Molecule=nn.Conv2d(64,64,kernel_size=3,stride=1,padding=1)#Input is 78, because this is what the 32+30+16 is.
-
-        self.pool2=nn.MaxPool2d(kernel_size=1,stride=2)
-
-        self.fc1=nn.Linear(16*784,32)#16*1911, since that is the shape of my data
-        self.fc2=nn.Linear(32,2)#Output layer is 2 because it is either present or not
+        pass
        
     def forward(self,x):
-
-        x=x.float()
-        x=x.view(32, 2, 1, 785)
-        # print(x)
-
-
-        x=self.pool(F.relu(self.conv1Molecule(x)))
-        x=self.pool2(F.relu(self.conv2Molecule(x)))
-
+        pass
         
-      
-        x=x.view(32,-1)#Flattens combindeX tensor
-        # print(combinedX.size())
-        # print(x.size())
-        x=F.relu(self.fc1(x))#Passes through first fully connected layer
-        output=self.fc2(x)
-
-        output=torch.sigmoid(output)
-        # print(output)
-        return output[:,1]
 
 
 
 torch.manual_seed(43)#So I can reproduce the results
 np.random.seed(43)
-molecule="Water"
-notName="Not"+molecule
-waterDataset=moleculeDataset(folderPath=r"C:\Users\Tristan\Downloads\ExoSeer\Data\Training"+f"\{molecule}",molecule=molecule,transform=transforms.ToTensor())
-notWaterDataset=moleculeDataset(folderPath=r"C:\Users\Tristan\Downloads\ExoSeer\Data\Training"+f"\{notName}",molecule=notName,transform=transforms.ToTensor())
-waterDataset=combineDataset(waterDataset,notWaterDataset)
 
-indexes=list(range(len(waterDataset)))
+combinations=[('N2',), ('O2',), ('CO2',), ('H2O',), ('N2', 'O2'), ('N2', 'CO2'), ('N2', 'H2O'), ('O2', 'CO2'), ('O2', 'H2O'), ('CO2', 'H2O'), ('N2', 'O2', 'CO2'), ('N2', 'O2', 'H2O'), ('N2', 'CO2', 'H2O'), ('O2', 'CO2', 'H2O'), ('N2', 'O2', 'CO2', 'H2O')]
+folderDirectories=[]
+folderPath=r'C:\Users\Tristan\Downloads\ExoSeer\Data\Training'
+for combination in combinations:
+    newPath=folderPath+f'\{"-".join(combination)}'
+    folderDirectories.append(("-".join(combination),newPath))
+
+dataset=customDataset(folderPaths=folderDirectories,transform=transforms.ToTensor())
+
+trainSize=int(0.8*len(dataset))
+testSize=(len(dataset)-trainSize)//2
+validationSize=(len(dataset)-trainSize)//2
 
 
-
-trainSize=int(0.8*len(waterDataset))
-testSize=(len(waterDataset)-trainSize)//2
-validationSize=(len(waterDataset)-trainSize)//2
-
-
-trainDataset,testDataset,validationDataset=random_split(waterDataset,[trainSize,testSize,validationSize])
+trainDataset,testDataset,validationDataset=random_split(dataset,[trainSize,testSize,validationSize])
 # print(len(validationDataset))
 
 
@@ -135,140 +114,157 @@ validationLoader=DataLoader(validationDataset,batch_size=32,shuffle=True)
 
 
 device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model=Model().to(device)
-
-optimizer=optim.Adam(model.parameters(),lr=0.001,weight_decay=0.001)
-# optimizer=optim.RMSprop(model.parameters(), lr=0.01)
-scheduler=torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=2, factor=0.0005)
-criterion=nn.BCELoss()
-
-
-n=15
-losses=[]
-validCorrect=0
-validTotal=0
-for epoch in range(n):
-    for batch in trainDataloader:
-        model.train()
-        rawData,labels=batch
-
-        rawData=normalize(rawData,0.0,1.0)#Normalizing
-        rawData=rawData.to(torch.float64)
+for batch in trainDataloader:
+    data,labels=batch[0],list(batch[1])
+    #Convert the labels into 1-hot encoding
+    #Each label will be an array that is 4 values long
+    #[0,0,0,0]
+    #The first value represents if N2 is present
+    #The second value represents if O2 is present
+    #The third value represents if CO2 is present
+    #The fourth value represents if H2O is present
+    for i,value in enumerate(labels):
+        labels[i]=oneHotEncoding(value)
+    print(data)
+    print(labels)
+    print(data.shape)
+    break
 
 
+# model=Model().to(device)
 
-        rawData=rawData.to(device)
+# optimizer=optim.Adam(model.parameters(),lr=0.001,weight_decay=0.001)
+# # optimizer=optim.RMSprop(model.parameters(), lr=0.01)
+# scheduler=torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=2, factor=0.0005)
+# criterion=nn.BCELoss()
 
 
-        newLabels=[]
-        for a in labels:
-            if a==molecule:
-                newLabels.append(1)
-            else:
-                newLabels.append(0)
-        labels=torch.tensor(newLabels)
-        labels=labels.float()
-        labels=labels.to(device)
+# n=15
+# losses=[]
+# validCorrect=0
+# validTotal=0
+# for epoch in range(n):
+#     for batch in trainDataloader:
+#         model.train()
+#         rawData,labels=batch
+
+#         rawData=normalize(rawData,0.0,1.0)#Normalizing
+#         rawData=rawData.to(torch.float64)
+
+
+
+#         rawData=rawData.to(device)
+
+
+#         newLabels=[]
+#         for a in labels:
+#             if a==molecule:
+#                 newLabels.append(1)
+#             else:
+#                 newLabels.append(0)
+#         labels=torch.tensor(newLabels)
+#         labels=labels.float()
+#         labels=labels.to(device)
         
-        optimizer.zero_grad()
-        outputs=model(rawData)
+#         optimizer.zero_grad()
+#         outputs=model(rawData)
 
         
-        loss=criterion(outputs,labels)
-        loss.backward()
-        optimizer.step()
+#         loss=criterion(outputs,labels)
+#         loss.backward()
+#         optimizer.step()
 
 
 
-    with torch.no_grad():#Validation loop
-        for batch in validationLoader:
-            model.eval()
-            rawData,labels=batch
-            mean_value=rawData.mean()
-            std_value=rawData.std()
-            rawData=(rawData-mean_value)/std_value#Normalizing
-            if list(rawData.size())[0]==32:#Otherwise errors, since dimensions don't match
-                rawData=rawData.to(device)
-                # meanData=meanData.to(device)
-                # downSampled=downSampled.to(device)
-                newLabels=[]
-                for a in labels:
-                    if a==molecule:
-                        newLabels.append(1)
-                    else:
-                        newLabels.append(0)
-                labels=torch.tensor(newLabels)
-                labels=labels.to(device)
-                outputs=model(rawData)
-                for i,a in enumerate(outputs):
-                    if a>0.5:#Water is present
-                        if labels[i]==1:
-                            validCorrect+=1
-                    else:
-                        if labels[i]==0:
-                            validCorrect +=1
-                    validTotal+=1
+#     with torch.no_grad():#Validation loop
+#         for batch in validationLoader:
+#             model.eval()
+#             rawData,labels=batch
+#             mean_value=rawData.mean()
+#             std_value=rawData.std()
+#             rawData=(rawData-mean_value)/std_value#Normalizing
+#             if list(rawData.size())[0]==32:#Otherwise errors, since dimensions don't match
+#                 rawData=rawData.to(device)
+#                 # meanData=meanData.to(device)
+#                 # downSampled=downSampled.to(device)
+#                 newLabels=[]
+#                 for a in labels:
+#                     if a==molecule:
+#                         newLabels.append(1)
+#                     else:
+#                         newLabels.append(0)
+#                 labels=torch.tensor(newLabels)
+#                 labels=labels.to(device)
+#                 outputs=model(rawData)
+#                 for i,a in enumerate(outputs):
+#                     if a>0.5:#Water is present
+#                         if labels[i]==1:
+#                             validCorrect+=1
+#                     else:
+#                         if labels[i]==0:
+#                             validCorrect +=1
+#                     validTotal+=1
 
 
 
-                labels=labels.float()
-                valLoss=criterion(outputs,labels)
+#                 labels=labels.float()
+#                 valLoss=criterion(outputs,labels)
 
-        # rawDimensions=list(rawData.size())
-        # meanDimensions=list(meanData.size())
-        # downSampledDimensions=list(downSampled.size())
-        # print(rawData)
-    print(f"Epoch {epoch+1}/{n}, Training Loss: {loss.item()}, Validation Loss: {valLoss.item()}, Validation Accuracy: {100 * validCorrect / validTotal}%")
-    losses.append(loss.item())
+#         # rawDimensions=list(rawData.size())
+#         # meanDimensions=list(meanData.size())
+#         # downSampledDimensions=list(downSampled.size())
+#         # print(rawData)
+#     print(f"Epoch {epoch+1}/{n}, Training Loss: {loss.item()}, Validation Loss: {valLoss.item()}, Validation Accuracy: {100 * validCorrect / validTotal}%")
+#     losses.append(loss.item())
 
-model.eval()
-correct=0
-total=0
-with torch.no_grad():
-    for batch,labels in testDataloader:
-        rawData=batch
-        if list(rawData.size())[0]==32:#Otherwise errors, since dimensions don't match
-            rawData=normalize(rawData,0.0,1.0)#Normalizing
-            rawData=rawData.to(torch.float64)
+# model.eval()
+# correct=0
+# total=0
+# with torch.no_grad():
+#     for batch,labels in testDataloader:
+#         rawData=batch
+#         if list(rawData.size())[0]==32:#Otherwise errors, since dimensions don't match
+#             rawData=normalize(rawData,0.0,1.0)#Normalizing
+#             rawData=rawData.to(torch.float64)
 
-            # meanData=meanFilter(rawData,3)
-            # meanData=normalize(meanData,0.0,1.0)
-            # meanData=meanData.to(torch.float64)
+#             # meanData=meanFilter(rawData,3)
+#             # meanData=normalize(meanData,0.0,1.0)
+#             # meanData=meanData.to(torch.float64)
 
-            # downSampled=rawData[::2]#Removes every 2.
-            # downSampled=normalize(downSampled,0.0,1.0)
-            # downSampled=downSampled.to(torch.float64)
+#             # downSampled=rawData[::2]#Removes every 2.
+#             # downSampled=normalize(downSampled,0.0,1.0)
+#             # downSampled=downSampled.to(torch.float64)
 
-            rawData=rawData.to(device)
-            # meanData=meanData.to(device)
-            # downSampled=downSampled.to(device)
-            newLabels=[]
-            for a in labels:
-                if a==molecule:
-                    newLabels.append(1)
-                else:
-                    newLabels.append(0)
-            labels=torch.tensor(newLabels)
-            labels=labels.float()
-            labels=labels.to(device)
-            outputs=model(rawData)
-            for i,a in enumerate(outputs):
-                if a>0.5:#Water is present
-                    if labels[i]==1:
-                        correct+=1
-                else:
-                    if labels[i]==0:
-                        correct +=1
-                total+=1
+#             rawData=rawData.to(device)
+#             # meanData=meanData.to(device)
+#             # downSampled=downSampled.to(device)
+#             newLabels=[]
+#             for a in labels:
+#                 if a==molecule:
+#                     newLabels.append(1)
+#                 else:
+#                     newLabels.append(0)
+#             labels=torch.tensor(newLabels)
+#             labels=labels.float()
+#             labels=labels.to(device)
+#             outputs=model(rawData)
+#             for i,a in enumerate(outputs):
+#                 if a>0.5:#Water is present
+#                     if labels[i]==1:
+#                         correct+=1
+#                 else:
+#                     if labels[i]==0:
+#                         correct +=1
+#                 total+=1
 
-cnnFilePath=r"C:\Users\Tristan\Downloads\ExoSeer"+f"\{molecule}CNN"
-torch.save(model.state_dict,cnnFilePath)
-print(f"Accuracy on the test set: {100 * correct / total}%") 
-plt.plot(losses)
-plt.ylabel("Training loss")
-plt.show(block=True)
-'''
-Few things left to do. First of all make sure this model sin't overfitting.
- Secondly figure out how I want to do the multiple CNN files. Whether or not i want them in different files.
- Then save the trained models, and work on the code to extract features and 
-'''
+# cnnFilePath=r"C:\Users\Tristan\Downloads\ExoSeer"+f"\{molecule}CNN"
+# torch.save(model.state_dict,cnnFilePath)
+# print(f"Accuracy on the test set: {100 * correct / total}%") 
+# plt.plot(losses)
+# plt.ylabel("Training loss")
+# plt.show(block=True)
+# '''
+# Few things left to do. First of all make sure this model sin't overfitting.
+#  Secondly figure out how I want to do the multiple CNN files. Whether or not i want them in different files.
+#  Then save the trained models, and work on the code to extract features and 
+# '''
